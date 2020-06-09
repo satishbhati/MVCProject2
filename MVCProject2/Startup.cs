647fc63eda887e4b86d3eb97668a38ab8994d8be
Revolution Trading <bhati4030@gmail.com>
2020-06-09 22:22:37 +0530

    Add project files.

---
+++ MVCProject2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVCProject2.Startup))]
+namespace MVCProject2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
